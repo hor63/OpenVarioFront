9336d9103a8e4b2ba78a0241f9be65384753ba7b
Kai Horstmann <horstmannkai@hotmail.com>
2024-10-16 17:11:22 +0200

    Start with class GLTextFontTexture

---
+++ src/GLTextRender/GLTextFontTexture.cpp
@@ -0,0 +1,55 @@
+/*
+ * GLTextFontTexture.cpp
+ *
+ *  Created on: Oct 16, 2024
+ *      Author: hor
+ *
+ *   Manages a GL texture which contains the images of a number of glyphs of a font.
+ *   The glyphs are arranged in lines with 1 pixel space to the edges and 2 pixels between the glyphs
+ *   Besides the actual texture which is managed by a GLTexture member the class stores the glyph
+ *   positions and sizes of the current line as well as the previous line.
+ *   This allows precise positioning of new glyhps above the glyphs of the previous line below.
+ *
+ *   This file is part of OpenVarioFront, an electronic variometer display for glider planes
+ *   Copyright (C) 2024 Kai Horstmann
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include "GLTextFontTexture.h"
+
+namespace OevGLES {
+
+GLTextFontTexture::GLTextFontTexture() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GLTextFontTexture::~GLTextFontTexture() {
+	// TODO Auto-generated destructor stub
+}
+
+GLTextFontTexture::GLTextFontTexture(GLTextFontTexture &&other) {
+	// TODO Auto-generated constructor stub
+
+}
+
+GLTextFontTexture::GLTextFontTexture(const GLTextFontTexture &other) {
+	// TODO Auto-generated constructor stub
+
+}
+
+} /* namespace OevGLES */
---
+++ src/GLTextRender/GLTextFontTexture.h
@@ -0,0 +1,75 @@
+/*
+ * GLTextFontTexture.h
+ *
+ *  Created on: Oct 16, 2024
+ *      Author: hor
+ *
+ *   Manages a GL texture which contains the images of a number of glyphs of a font.
+ *   The glyphs are arranged in lines with 1 pixel space to the edges and 2 pixels between the glyphs
+ *   Besides the actual texture which is managed by a GLTexture member the class stores the glyph
+ *   positions and sizes of the current line as well as the previous line.
+ *   This allows precise positioning of new glyhps above the glyphs of the previous line below.
+ *
+ *   This file is part of OpenVarioFront, an electronic variometer display for glider planes
+ *   Copyright (C) 2024 Kai Horstmann
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef GLTEXTRENDER_GLTEXTFONTTEXTURE_H_
+#define GLTEXTRENDER_GLTEXTFONTTEXTURE_H_
+
+#include <cstdint>
+#include <forward_list>
+
+#include <GLTextFontCache.h>
+
+namespace OevGLES {
+
+class GLTextFontCacheItem;
+
+class GLTextFontTexture {
+public:
+
+	struct GlyphBox {
+		PangoGlyph glyphIndex;
+
+		uint16_t x, y;
+		uint16_t heigth, width;
+	};
+
+	using GlyphBoxList = std::forward_list<TextureGlyphBox>;
+
+	GLTextFontTexture();
+	GLTextFontTexture(const GLTextFontTexture &other) = delete;
+	GLTextFontTexture(GLTextFontTexture &&other);
+	virtual ~GLTextFontTexture();
+	GLTextFontTexture& operator=(const GLTextFontTexture &other) = delete;
+	GLTextFontTexture& operator=(GLTextFontTexture &&other);
+
+private:
+
+	GLTextFontCacheItem& fontCacheItem;
+
+	GlyphBoxList previousLine;
+	GlyphBoxList currentLine;
+};
+
+
+
+} /* namespace OevGLES */
+
+#endif /* GLTEXTRENDER_GLTEXTFONTTEXTURE_H_ */
