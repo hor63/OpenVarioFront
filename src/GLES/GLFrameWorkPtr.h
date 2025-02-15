29ba2bd066a7c4a7da7178ed7f48dd2ae4b88258
Kai Horstmann <horstmannkai@hotmail.com>
2025-02-15 02:22:32 +0100

    src/GLES/GLFrameWorkPtr.h: Add license comment.

--- src/GLES/GLFrameWorkPtr.h
+++ src/GLES/GLFrameWorkPtr.h
@@ -3,6 +3,27 @@
  *
  *  Created on: Feb 8, 2025
  *      Author: hor
+ *
+ * Forward and type alias declarations in a separate header file to avoid issues
+ * with circular includes of GLFramework and subordinate classes
+ *
+ *   This file is part of OpenVarioFront, an electronic variometer display for glider planes
+ *   Copyright (C) 2024 Kai Horstmann
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
  */
 
 #ifndef GLES_GLFRAMEWORKPTR_H_
@@ -11,6 +32,8 @@
 #include <memory>
 
 namespace OevGLES {
+
+
 
 class GLFramework;
 
