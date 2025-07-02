bdca2c076598e63feef338e2057d837ebfb36ddf
Kai Horstmann <horstmannkai@hotmail.com>
2025-07-02 16:44:06 +0200

    ControlBase: Empty shell class

---
+++ src/lib/Controls/ControlBase.cpp
@@ -0,0 +1,21 @@
+/*
+ * ControlBase.cpp
+ *
+ *  Created on: Jul 2, 2025
+ *      Author: hor
+ */
+
+#include "lib/Controls/ControlBase.h"
+
+namespace OevControls {
+
+ControlBase::ControlBase() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ControlBase::~ControlBase() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace OevControls */
---
+++ src/lib/Controls/ControlBase.h
@@ -0,0 +1,21 @@
+/*
+ * ControlBase.h
+ *
+ *  Created on: Jul 2, 2025
+ *      Author: hor
+ */
+
+#ifndef LIB_CONTROLS_CONTROLBASE_H_
+#define LIB_CONTROLS_CONTROLBASE_H_
+
+namespace OevControls {
+
+class ControlBase {
+public:
+	ControlBase();
+	virtual ~ControlBase();
+};
+
+} /* namespace OevControls */
+
+#endif /* LIB_CONTROLS_CONTROLBASE_H_ */
