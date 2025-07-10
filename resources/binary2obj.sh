94e536ef201a9db2fe0da180fc7e2dca79ecdb80
Kai Horstmann <horstmannkai@hotmail.com>
2025-07-10 16:57:24 +0200

    resources/: Stream process. Add JPEG files, generate file name for user
    Rename the generated variables to <NAME>_data and <NAME>_data_end.
    Add <NAME>_filename and <NAME>_filename_length.
    <NAME>_filename_length is the length exclufing the terminating \0.
    
    Streamline the interaction between Makefile.am, binary2obj.sh and
    includeBinary.S.

--- resources/binary2obj.sh
+++ resources/binary2obj.sh
@@ -6,52 +6,61 @@
 #
 # Enviroment variable: srcdir: Directory there Makefile.am resides.
 
-inputFile=$2
-outputFile=$1
-fileExtension=$3
+inputFile="$2"
+outputFile="$1"
+fileStem="$3"
 
-baseName=`basename "$inputFile" $fileExtension`
+srcdir=`dirname "$0"`
+baseStem=`basename "$fileStem"`
+baseName=`basename "$inputFile"`
 outputDirectory=`dirname "$outputFile"`
-outputHeaderFile=${outputDirectory}/${baseName}.h
+outputHeaderFile="${outputDirectory}/${baseStem}.h"
 
-echo "inputFile = $inputFile"
-echo "outputFile = $outputFile"
-echo "fileExtension = $fileExtension"
-echo "baseName = $baseName"
-echo "outputDirectory = $outputDirectory"
-echo "outputHeaderFile = $outputHeaderFile"
+# echo "inputFile = $inputFile"
+# echo "outputFile = $outputFile"
+# echo "fileStem = $fileStem"
+# echo "baseStem = $baseStem"
+# echo "srcdir = $srcdir"
+# echo "outputDirectory = $outputDirectory"
+# echo "outputHeaderFile = $outputHeaderFile"
 
-echo "${CCAS} ${CCASFLAGS} -c -o ${outputFile} -DNAME=\"${baseName}\" -DFILE_NAME=\"${inputFile}\" \"${srcdir}/includeBinary.S\""
-${CCAS} ${CCASFLAGS} -c -o ${outputFile} -DNAME="${baseName}" -DFILE_NAME="${inputFile}" "${srcdir}/includeBinary.S"
+# echo "${CCAS} ${CCASFLAGS} -c -o ${outputFile} -DNAME=\"${baseStem}\" -DFILE_NAME=\"${inputFile}\" \"${srcdir}/includeBinary.S\""
+${CCAS} ${CCASFLAGS} -c -o "${outputFile}" -DNAME="${baseStem}" \
+	-DFILE_NAME="${inputFile}" -DBASE_FILENAME="${baseName}"\
+	"${srcdir}/includeBinary.S"
 
-echo "/* file: `basename $outputHeaderFile`
+echo "/* file: `basename "$outputHeaderFile"`
  *
  * Generated header for access to the in-memory data of file
  * \"$inputFile\"
  *
  * To refer to and use the declarations below you must link object file
- * ${outputFile} 
+ * \"${outputFile}\" 
  *
  */
 
-#if !${baseName}_H_INCLUDED
+#if !${baseStem}_H_INCLUDED
 
-#define ${baseName}_H_INCLUDED 1
+#define ${baseStem}_H_INCLUDED 1
 
 #if defined __cplusplus
 extern \"C\" {
 #endif
 
 /* Points to the start of the binary content */
-extern const char $baseName [];
+extern const char ${baseStem}_data [];
 /* Points to the address after the memory content. */
-extern const char ${baseName}_end [];
+extern const char ${baseStem}_data_end [];
 /* Size in bytes of the memory content */
-extern const int ${baseName}_size;
+extern const int ${baseStem}_size;
+
+extern const char ${baseStem}_filename [];
+/* The length of ${baseStem}_filename without the terminating \\0 */
+extern int ${baseStem}_filename_length;
 	
 #if defined __cplusplus
 } /* extern \"C\" { */
 #endif
 
-#endif /* #if !${baseName}_H_INCLUDED */
+#endif /* #if !${baseStem}_H_INCLUDED */
 " > ${outputHeaderFile}
