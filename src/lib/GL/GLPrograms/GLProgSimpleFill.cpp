1d4a16622e9a5edb76b31a39b2fff88f1fc3252f
Kai Horstmann <horstmannkai@hotmail.com>
2025-08-07 12:54:20 +0200

    Add shader program GLProgSimpleFill.
    This program provides simple filling with an ambient coloring only.

---
+++ src/lib/GL/GLPrograms/GLProgSimpleFill.cpp
@@ -0,0 +1,120 @@
+/*
+ * GLProgSimpleFill.cpp
+ *
+ *  Created on: May 16, 2018
+ *      Author: hor
+ *
+ *   This file is part of OpenVarioFront, an electronic variometer display for glider planes
+ *   Copyright (C) 2018  Kai Horstmann
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+
+#include "GLPrograms/GLProgSimpleFill.h"
+
+namespace OevGLES {
+
+GLProgSimpleFill* GLProgSimpleFill::theProgram = 0;
+
+GLProgSimpleFill::~GLProgSimpleFill() {
+
+	// This deletes the only instance of the program
+	theProgram = 0;
+
+}
+
+GLProgSimpleFill* GLProgSimpleFill::getProgram() {
+
+	if (!theProgram) {
+		theProgram = new GLProgSimpleFill;
+
+		theProgram->createProgram();
+	}
+
+	return theProgram;
+
+}
+
+void GLProgSimpleFill::destroyProgram() {
+	if (theProgram) {
+		delete theProgram;
+		theProgram = 0;
+	}
+}
+
+const char* GLProgSimpleFill::getVertexShaderCode() const {
+
+	return
+R"(
+#version 100
+
+precision mediump float;
+
+// MVP matrix is used to transform points
+uniform mat4 mvpMatrix;
+
+uniform vec4 ambientLightColor;
+
+attribute vec4 vertexPos;
+attribute vec4 vertexColor;
+
+varying vec4 varFragColor;
+
+void main () { 
+	vec4 lightColor = ambientLightColor;
+	
+	lightColor.a = 1.0;
+	varFragColor = vertexColor * lightColor;
+	gl_Position = mvpMatrix * vertexPos;
+}
+)";
+
+}
+
+const char* GLProgSimpleFill::getFragmentShaderCode() const {
+
+	return
+R"(
+#version 100
+
+precision mediump float;
+varying vec4 varFragColor;
+
+void main () {
+	gl_FragColor = varFragColor;
+};
+)";
+}
+
+
+void OevGLES::GLProgSimpleFill::retrieveShaderVariableInfo() {
+
+	// The uniforms
+	mvpMatrixInfo			= *retrieveSingleUniformInfo("mvpMatrix",mvpMatrixLocation);
+	ambientLightColorInfo	= *retrieveSingleUniformInfo("ambientLightColor",ambientLightColorLocation);
+
+	// The vertex attributes
+	vertexPosInfo		= *retrieveSingleAttributeInfo("vertexPos",vertexPosLocation);
+	vertexColorInfo		= *retrieveSingleAttributeInfo("vertexColor",vertexColorLocation);
+
+}
+
+} /* namespace OevGLES */
