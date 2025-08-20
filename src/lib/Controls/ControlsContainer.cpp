025f10b1cd93add21b912fb7b53465ad0e613a6a
Kai Horstmann <horstmannkai@hotmail.com>
2025-08-20 17:47:29 +0200

    ControlsContainer: Add append method for the tab group
    Make the container members protected, and directly accessible for
    subclasses.
    The add and append methods accept rvalues.

--- src/lib/Controls/ControlsContainer.cpp
+++ src/lib/Controls/ControlsContainer.cpp
@@ -3,12 +3,34 @@
  *
  *  Created on: Aug 14, 2025
  *      Author: hor
+ *
+ *	Base class for any control which can host other controls, and a tab group,
+ *  like dialogs, canvases, and the root control
+ *
+ *   This file is part of OpenVarioFront, an electronic variometer display for glider planes
+ *   Copyright (C) 2018  Kai Horstmann
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
  */
-#include <utility>
 #ifdef HAVE_CONFIG_H
 #  include <config.h>
 #endif
 #include "OVFCommon.h"
+
+#include <utility>
 
 #include "ControlsContainer.h"
 
@@ -22,8 +44,17 @@ ControlsContainer::~ControlsContainer() {
 	// TODO Auto-generated destructor stub
 }
 
-void ControlsContainer::addControl(ControlBasePtr controlPtr) {
-	controlsMap.insert(std::make_pair(controlPtr->getUuid(), controlPtr));
+void ControlsContainer::addControl(ControlBasePtr &&controlPtr) {
+	controlsMap.insert(std::pair(controlPtr->getUuid(),controlPtr));
+}
+
+void ControlsContainer::appendControlToTabGroup (ControlBaseWeakPtr &&controlWeakPtr) {
+	tabGroup.push_back(controlWeakPtr);
+}
+
+void ControlsContainer::insertControlInTabGroupBefore (ControlsWeakListT::iterator ref,
+		ControlBaseWeakPtr &&controlWeakPtr) {
+	tabGroup.insert (ref,controlWeakPtr);
 }
 
 } /* namespace OevControls */
--- src/lib/Controls/ControlsContainer.h
+++ src/lib/Controls/ControlsContainer.h
@@ -3,6 +3,27 @@
  *
  *  Created on: Aug 14, 2025
  *      Author: hor
+ *
+ *	Base class for any control which can host other controls, and a tab group,
+ *  like dialogs, canvases, and the root control
+ *
+ *   This file is part of OpenVarioFront, an electronic variometer display for glider planes
+ *   Copyright (C) 2018  Kai Horstmann
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
  */
 
 #ifndef LIB_CONTROLS_CONTROLSCONTAINER_H_
@@ -18,6 +39,10 @@ namespace OevControls {
 
 class ControlsContainer: public ControlBase {
 public:
+
+	using ControlsMapT = std::unordered_map<OevUtil::Uuid, ControlBasePtr>;
+	using ControlsWeakListT = std::list<ControlBaseWeakPtr>;
+
 	ControlsContainer();
 	virtual ~ControlsContainer();
 	ControlsContainer(const ControlsContainer &other) = delete;
@@ -25,13 +50,16 @@ public:
 	ControlsContainer& operator=(const ControlsContainer &other) = delete;
 	ControlsContainer& operator=(ControlsContainer &&other) = delete;
 	
-	void addControl(ControlBasePtr controlPtr);
+	void addControl(ControlBasePtr &&controlPtr);
+	void appendControlToTabGroup (ControlBaseWeakPtr &&controlWeakPtr);
+	void insertControlInTabGroupBefore (ControlsWeakListT::iterator ref,
+		ControlBaseWeakPtr &&controlWeakPtr);
 	
-private:
+protected:
 
-std::unordered_map<OevUtil::Uuid, ControlBasePtr> controlsMap;
+ControlsMapT controlsMap;
 
-std::list<ControlBaseWeakPtr> tabGroup;
+ControlsWeakListT tabGroup;
 
 };
 
