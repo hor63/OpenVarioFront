e54d15eeb7da1ac1105758ae9f1392d3677154df
Kai Horstmann <horstmannkai@hotmail.com>
2025-09-22 02:03:24 +0200

    RendererBase.h - RenderStandardUniforms: Comprehensive Doxygen documentation.

--- src/lib/GL/Renderers/RendererBase.h
+++ src/lib/GL/Renderers/RendererBase.h
@@ -153,9 +153,11 @@ using BlendAttributeSetRestoreStd =
 	
 	The class stores shared pointers to the uniform data.
 	This makes it economical to copy objects because they reference to the same
-	data, and also makes it easy to inherit shared data.
+	data.
+	Thus copying such an object shares the uniform data, i.e. change one of the
+	uniforms in one objects, the change applies to all copies.
 	
-	You can replace shared pointers with the reset routines.
+	You can replace shared pointers with the reset... method.
 	The reset routines will also create new instances of the MV or MVP matrixes
 	when necessary.
 	Once you replace a shared pointer with a new one the attribute will become
@@ -169,6 +171,11 @@ using BlendAttributeSetRestoreStd =
 class RenderStandardUniforms {
 public:
 
+	/** \brief Combines a 4x4 matrix with a change sequence counter
+	 
+	 The change counter is used for shared objects to indicate a change of
+	 content for consumers of the matrix.
+	*/
 	struct Mat4WithChangeCounter {
 		Mat4 matrix4;
 		int32_t changeCounter;
@@ -180,69 +187,325 @@ public:
 	using Mat4WithChangeCounterPtr = std::shared_ptr<Mat4WithChangeCounter>;
 
 	RenderStandardUniforms();
-
+	
+	/** \brief Writable access to the model matrix
+	 
+	 Upon calling the changeCounter is being updated to indicate a possible
+	 change of the content.
+	 
+	 Use only when you intend to modify the content.
+	 Calling this method for read access only works of course but is inefficient
+	 because it forces a re-caculation of \ref MVMatrixPtr and
+	 \ref MVPMatrixPtr contents when you access them.
+	 If you only want to read the content call \ref getModelMatrixC instead.
+	 	 
+	 \see \ref modelMatrixPtr
+	 \see \ref getModelMatrixC read-only access()
+	*/
 	Mat4 & getModelMatrix ();
+	
+	/** \brief Writable access to the view matrix
+	 
+	 Upon calling the changeCounter is being updated to indicate a possible
+	 change of the content.
+	 
+	 Use only when you intend to modify the content. If you only want to read
+	 the content call \ref getViewMatrixC.
+	 Calling this method for read access only works of course but is inefficient
+	 because it forces a re-caculation of \ref MVMatrixPtr and
+	 \ref MVPMatrixPtr contents when you access them.
+	 
+	 \see \ref viewMatrixPtr
+	 \see \ref getViewMatrixC() read-only access
+	*/
 	Mat4 & getViewMatrix ();
+	
+	/** \brief Writable access to the projection matrix
+	 
+	 Upon calling the changeCounter is being updated to indicate a possible
+	 change of the content.
+	 
+	 Use only when you intend to modify the content. If you only want to read
+	 the content call \ref getProjMatrixC.
+	 Calling this method for read access only works of course but is inefficient
+	 because it forces a re-caculation of
+	 \ref MVPMatrixPtr contents when you access them.
+	 
+	 \see \ref projMatrixPtr
+	 \see \ref getProjMatrixC() read-only access
+	*/
 	Mat4 & getProjMatrix ();
+	
 	// There is no writable access to the MV and MVP matrixes.
 	// They are derived from the M, V and P matrixes automatically.
 	
+	/** \brief Writable access to the light direction vector.
+	 
+	 \see \ref lightDirPtr
+	 \see \ref getLightDirC() read-only access
+	*/
 	Vec3 & getLightDir () {
 		return *lightDirPtr;
 	}
+	
+	/** \brief Writable access to the light color array.
+	 
+	 \see \ref lightColorPtr
+	 \see \ref getLightColorC() read-only access
+	*/
 	Vec4 & getLightColor () {
 		return *lightColorPtr;
 	}
+	
+	/** \brief Writable access to the ambient light color array.
+	 
+	 \see \ref ambientLightColorPtr
+	 \see \ref getAmbientLightColorC() read-only access
+	*/
 	Vec4 & getAmbientLightColor () {
 		return *ambientLightColorPtr;
 	}
 
+	/** \brief Read-only access to the model matrix.
+	 
+	 \see \ref modelMatrixPtr
+	 \see \ref getModelMatrix() read/write access
+	/** \brief Read-only access to the light color array.
+	 
+	 \see \ref lightColorPtr
+	 \see \ref getLightColor() read/write access
+	*/
+
+	*/
 	Mat4 const & getModelMatrixC () const {
 		return modelMatrixPtr->matrix4;
 	}
+
+	/** \brief Read-only access to the view matrix.
+	 
+	 \see \ref viewMatrixPtr
+	 \see \ref getviewMatrix() read/write access
+	*/
 	Mat4 const & getViewMatrixC () const {
 		return viewMatrixPtr->matrix4;
 	}
+
+	/** \brief Read-only access to the projection matrix.
+	 
+	 \see \ref projMatrixPtr
+	 \see \ref getprojMatrix() read/write access
+	*/
 	Mat4 const & getProjMatrixC () const {
 		return projMatrixPtr->matrix4;
 	}
+
+	/** \brief Read-only access to the model-view matrix.
+	 
+	 \ref MVMatrixPtr is automatically re-calculated when \ref modelMatrixPtr
+	 and/or \ref viewMatrixPtr have been modified by calls to 
+	 \ref getModelMatrix() or \ref getViewMatix().
+	 
+	 Therefore \ref MVMatrixPtr is declared mutable to be updated also for
+	 constant objects.
+	 
+	 There is no writable access method for \ref MVMatrixPtr because it is a
+	 derived value, and calculated automatically.
+	 
+	 \see \ref MVMatrixPtr
+	*/
 	Mat4 const &getMVMatrixC() const;
+
+	/** \brief Read-only access to the model-view-projection matrix.
+	 
+	 \ref MVPMatrixPtr is automatically re-calculated when \ref modelMatrixPtr
+	 and/or \ref viewMatrixPtr and/or \ref projMatrixPtr have been modified by
+	 calls to \ref getModelMatrix() or \ref getViewMatix() or
+	 \ref getProjMatrix().
+	 
+	 Therefore \ref MVPMatrixPtr is declared mutable to be updated also for
+	 constant objects.
+	 
+	 There is no writable access method for \ref MVPMatrixPtr because it is a
+	 derived value, and calculated automatically.
+	 
+	 \see \ref MVPMatrixPtr
+	*/
 	Mat4 const &getMVPMatrixC() const;
+
+	/** \brief Read-only access to the light direction vector.
+	 
+	 \see \ref lightDirPtr
+	 \see \ref getLightDir() read/write access
+	*/
 	Vec3 const &getLightDirC() const {
 		return *lightDirPtr;
 	}
+
+	/** \brief Read-only access to the light color array.
+	 
+	 \see \ref lightColorPtr
+	 \see \ref getLightColor() read/write access
+	*/
 	Vec4 const & getLightColorC () const {
 		return *lightColorPtr;
 	}
+
+	/** \brief Read-only access to the ambient light color array.
+	 
+	 \see \ref ambientLightColorPtr
+	 \see \ref getAmbientLightColor() read/write access
+	*/
 	Vec4 const & getAmbientLightColorC () const {
 		return *ambientLightColorPtr;
 	}
 
+	/** Create a copy of the model matrix, and store it in a new shared pointer.
+	 
+	 This method un-shares the model matrix with all other copies of \p this.
+	 The value of the model matrix remains the same.
+	 But changes to it will not affect any previous copies of \p this.
+	 To keep the derived matrixes consistent \ref resetMVMatrix() and 
+	 \ref resetMVPMatrix() are called to create new independent instances of
+	 these shared pointers and their data too.
+	
+	\see \ref modelMatrixPtr
+	*/
 	void resetModelMatrixPtr();
+
+	/** Create a copy of the view matrix, and store it in a new shared pointer.
+	 
+	 This method un-shares the view matrix with all other copies of \p this.
+	 The value of the view matrix remains the same.
+	 But changes to it will not affect any previous copies of \p this.
+	 To keep the derived matrixes consistent \ref resetMVMatrix() and 
+	 \ref resetMVPMatrix() are called to create new independent instances of
+	 these shared pointers and their data too.
+	
+	\see \ref viewMatrixPtr
+	*/
 	void resetViewMatrixPtr();
+
+	/** Create a copy of the projection matrix, and store it in a new shared pointer.
+	 
+	 This method un-shares the projection matrix with all other copies of \p this.
+	 The value of the projection matrix remains the same.
+	 But changes to it will not affect any previous copies of \p this.
+	 To keep the derived matrixes consistent 
+	 \ref resetMVPMatrix() is called to create new independent instance of
+	 that shared pointer and its data too.
+	
+	\see \ref viewMatrixPtr
+	*/
 	void resetProjMatrixPtr();
+
+	/** Create a copy of the light direction vector, and store it in a new
+	 shared pointer.
+	 
+	 This method un-shares the light direction vector with all other copies 
+	 of \p this.
+	 The value of the light direction vector remains the same.
+	 But changes to it will not affect any previous copies of \p this.
+	
+	\see \ref lightDirPtr
+	*/
 	void resetLightDirPtr();
+
+	/** Create a copy of the light color array, and store it in a new
+	 shared pointer.
+	 
+	 This method un-shares the light color array with all other copies 
+	 of \p this.
+	 The value of the light color array remains the same.
+	 But changes to it will not affect any previous copies of \p this.
+	
+	\see \ref lightColorPtr
+	*/
 	void resetLightColorPtr();
+
+	/** Create a copy of the ambient light color array, and store it in a new
+	 shared pointer.
+	 
+	 This method un-shares the ambient light color array with all other copies 
+	 of \p this.
+	 The value of the ambient light color array remains the same.
+	 But changes to it will not affect any previous copies of \p this.
+	
+	\see \ref AmbientLightColorPtr
+	*/
 	void resetAmbientLightColorPtr();
 
+	/** \brief Recalculate the model-view matrix calling also 
+	 \ref recalcMVPMatrix()
+	 
+	 Despite declared const it changes \ref MVMatrix which is therefore
+	 declared mutable
+	*/
 	void recalcMVMatrix() const;
 	
+	/** \brief Recalculate the model-view-projection matrix
+	 
+	 Despite declared const it changes \ref MVPMatrix which is therefore
+	 declared mutable
+	*/
 	void recalcMVPMatrix() const;
 
 private:
 
 	static int32_t maxMatrixChangeCounter;
 
+	/// \brief Initialized to unity matrix.
 	Mat4WithChangeCounterPtr modelMatrixPtr;
+	/// \brief Initialized to unity matrix.
 	Mat4WithChangeCounterPtr viewMatrixPtr;
+	/// \brief Initialized to unity matrix.
 	Mat4WithChangeCounterPtr projMatrixPtr;
+	
+	/** \brief Initialized to unity matrix.
+	 
+	 It is declared mutable because it is called indirectly from
+	 \ref getMVMatrixC() or \ref getMVPMatrixC()
+	 */
 	mutable Mat4WithChangeCounterPtr MVMatrixPtr;
+	
+	/** \brief Initialized to unity matrix.
+	 
+	 It is declared mutable because it is called indirectly from
+	 \ref getMVMatrixC() or \ref getMVPMatrixC()
+	 */
 	mutable Mat4WithChangeCounterPtr MVPMatrixPtr;
 	Vec3ShPtr lightDirPtr;
 	Vec4ShPtr lightColorPtr;
 	Vec4ShPtr ambientLightColorPtr;
 
+	/** Create a copy of the model-view matrix, and store it in a new shared pointer.
+	 
+	 This method un-shares the MV matrix with all other copies of \p this.
+	 The value of the MV matrix remains the same.
+	 But changes to it will not affect any previous copies of \p this.
+	 To keep the derived matrixes consistent
+	 
+	 \ref resetMVPMatrix() is called to create new independent instance of
+	 that shared pointer and its data too.
+	 
+	 It is private because the MV matrix is a derived value, and must not be
+	 writeable accessible from outside.
+	
+	\see \ref MVMatrixPtr
+	*/
 	void resetMVMatrix();
+
+	/** Create a copy of the model-view-projection matrix, and store it in a new
+	 shared pointer.
+	 
+	 This method un-shares the MVP matrix with all other copies of \p this.
+	 The value of the MVP matrix remains the same.
+	 But changes to it will not affect any previous copies of \p this.
+	 
+	 It is private because the MV matrix is a derived value, and must not be
+	 writeable accessible from outside.
+	
+	\see \ref MVPMatrixPtr
+	*/
 	void resetMVPMatrix();
 };
 
